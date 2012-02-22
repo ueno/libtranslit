7ae3c98308e48780e4f3a10596bbea667a3ee6b9
Daiki Ueno <ueno@unixuser.org>
2012-02-22 13:06:08 +0900

    Merge translitmodule.[ch] into translitfilter.c.

--- libtranslit/translitfilter.c
+++ libtranslit/translitfilter.c
@@ -18,7 +18,6 @@
 
 #include <gio/gio.h>
 #include <libtranslit/translit.h>
-#include <libtranslit/translitmodule.h>
 
 enum
   {
@@ -37,6 +36,128 @@ struct _TranslitFilterPrivate
   char *language;
   char *name;
 };
+
+typedef struct _TranslitModule TranslitModule;
+typedef struct _TranslitModuleClass TranslitModuleClass;
+
+struct _TranslitModule
+{
+  GTypeModule parent;
+  gchar *filename;
+  GModule *library;
+  gboolean initialized;
+
+  void (*load)   (TranslitModule *module);
+  void (*unload) (TranslitModule *module);
+};
+
+struct _TranslitModuleClass
+{
+  GTypeModuleClass parent_class;
+};
+
+#define TRANSLIT_TYPE_MODULE (translit_module_get_type())
+#define TRANSLIT_MODULE(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), TRANSLIT_TYPE_MODULE, TranslitModule))
+#define TRANSLIT_MODULE_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), TRANSLIT_TYPE_MODULE, TranslitModuleClass))
+#define TRANSLIT_IS_MODULE(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), TRANSLIT_TYPE_MODULE))
+#define TRANSLIT_IS_MODULE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), TRANSLIT_TYPE_MODULE))
+#define TRANSLIT_MODULE_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), TRANSLIT_TYPE_MODULE, TranslitModuleClass))
+
+static GType           translit_module_get_type (void) G_GNUC_CONST;
+static TranslitModule *translit_module_new      (const gchar *filename);
+
+G_DEFINE_TYPE (TranslitModule, translit_module, G_TYPE_TYPE_MODULE);
+
+static gboolean
+translit_module_real_load (GTypeModule *gmodule)
+{
+  TranslitModule *module = TRANSLIT_MODULE (gmodule);
+
+  g_return_val_if_fail (module->filename, FALSE);
+
+  module->library = g_module_open (module->filename,
+				   G_MODULE_BIND_LAZY | G_MODULE_BIND_LOCAL);
+
+  if (!module->library)
+    {
+      g_printerr ("%s\n", g_module_error ());
+      return FALSE;
+    }
+
+  /* Make sure that the loaded library contains the required methods */
+  if (! g_module_symbol (module->library,
+                         "translit_module_load",
+                         (gpointer) &module->load) ||
+      ! g_module_symbol (module->library,
+                         "translit_module_unload",
+                         (gpointer) &module->unload))
+    {
+      g_printerr ("%s\n", g_module_error ());
+      g_module_close (module->library);
+
+      return FALSE;
+    }
+
+  /* Initialize the loaded module */
+  module->load (module);
+  module->initialized = TRUE;
+
+  return TRUE;
+}
+
+static void
+translit_module_real_unload (GTypeModule *gmodule)
+{
+  TranslitModule *module = TRANSLIT_MODULE (gmodule);
+
+  module->unload (module);
+
+  g_module_close (module->library);
+  module->library = NULL;
+
+  module->load   = NULL;
+  module->unload = NULL;
+}
+
+static void
+translit_module_finalize (GObject *object)
+{
+  TranslitModule *module = TRANSLIT_MODULE (object);
+
+  g_free (module->filename);
+
+  G_OBJECT_CLASS (translit_module_parent_class)->finalize (object);
+}
+
+static void
+translit_module_class_init (TranslitModuleClass *class)
+{
+  GTypeModuleClass *type_module_class = G_TYPE_MODULE_CLASS (class);
+  GObjectClass *object_class = G_OBJECT_CLASS (class);
+
+  type_module_class->load = translit_module_real_load;
+  type_module_class->unload = translit_module_real_unload;
+
+  object_class->finalize = translit_module_finalize;
+}
+
+static void
+translit_module_init (TranslitModule *module)
+{
+}
+
+static TranslitModule *
+translit_module_new (const gchar *filename)
+{
+  TranslitModule *module;
+
+  g_return_val_if_fail (filename != NULL, NULL);
+
+  module = g_object_new (TRANSLIT_TYPE_MODULE, NULL);
+  module->filename = g_strdup (filename);
+
+  return module;
+}
 
 static GHashTable *filters = NULL;
 static GHashTable *filter_types = NULL;
